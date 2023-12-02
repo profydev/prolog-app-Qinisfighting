5939c8b3acd43cf0d5dcb11b40a634af6dcac8ba
Yanqin Qualmann <95076245+Qinisfighting@users.noreply.github.com>
2023-12-02 08:48:47 +0100

    Footer (#10)
    * task1
    
    * task1
    
    * task2
    
    * task3
    
    * task4
    
    * task5
    
    * task5
    
    * task5
    
    * task5
    
    * task6
    
    * task7
    
    * change user number
    
    * fix statues
    
    * statue test
    
    * add footer
    
    * add footer

---
+++ features/projects/components/footer/Footer.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
+import { version } from "package.json";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div id="footer" className={styles.footer}>
+      <p id="version" className={styles.version}>
+        Version:{version}
+      </p>
+      <div id="footerMenuItems" className={styles.footerMenuItems}>
+        <a href="#">Docs</a>
+        <a href="#">API</a>
+        <a href="#">Help</a>
+        <a href="#">Community</a>
+      </div>
+
+      <img
+        id="footerLogo"
+        className={styles.logo}
+        src="/icons/logo-small.svg"
+        alt="Logo"
+      />
+    </div>
+  );
+};
+
+export default Footer;
